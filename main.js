909b2138c46cedadf8bf7be5edbbf519f27e6362
NagaRithwik <89988578+NagaRithwik@users.noreply.github.com>
2021-09-02 20:01:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function phonelap(){
+    window.location = "phonelap.html";
+}
+function tvac(){
+    window.location = "tvac.html";
+}
+function humans(){
+    window.location = "humans.html";
+}
+function bookpen(){
+    window.location = "bookpen.html";
+}
+function bottles(){
+    window.location = "bottles.html";
+}
---
+++ phonelap.js
@@ -0,0 +1,18 @@
+img="";
+objects=[];
+status="";
+
+function preload(){
+  img=loadImage('lapone.jpg');
+}
+function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+}
+function draw(){
+    image(img,0,0,550,480);
+    fill("red");
+    nofill();
+    stroke("red");
+    rect();
+}
