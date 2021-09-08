3b31e4d720942cacf36c45dae690df5014c67d47
NagaRithwik <89988578+NagaRithwik@users.noreply.github.com>
2021-09-08 19:09:50 +0530

    Add files via upload

--- humans.js
+++ humans.js
@@ -6,13 +6,45 @@ function preload(){
   img=loadImage('human.jpg');
 }
 function setup(){
-    canvas = createCanvas(500,400);
+    canvas = createCanvas(500,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status :- detecting objects";
+}
+function modelLoaded(){
+  console.log("Model Loaded!");
+  status = true;
+  objectDetector.detect(img , gotResults);
+}
+function gotResults(error,results){
+  if(error){
+    console.log(error);
+  }
+    console.log(results);
+    objects = results;
 }
 function draw(){
-    image(img,0,0,550,480);
-    fill("red");
-    nofill();
-    stroke("red");
-    rect();
+    image(img,0,0,500,420);
+   
+    if(status != ""){
+      for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML = "Status :- Object Detected";
+        fill("red");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
+        noFill();
+        stroke("red");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+      }
+      for(j=0;i<objects.length;j++){
+        document.getElementById("status").innerHTML = "Status :- Object Detected";
+        fill("red");
+        percent = floor(objects[j].confidence * 100);
+        text(objects[j].label+""+percent+"%",objects[j].x+15,objects[i].y+15);
+        noFill();
+        stroke("red");
+        rect(objects[j].x,objects[j].y,objects[j].width,objects[j].height);
+      }
+    }
+    
 }
