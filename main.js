bce8af276c9ef2f2bf7d6c23673c3e010ddef41c
NagaRithwik <89988578+NagaRithwik@users.noreply.github.com>
2021-09-09 19:15:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
-status = "";
-video = "";
+song="";
+objects=[];
+status="";
 
 function preload(){
-   video = loadVideo('video.mp4');
-   video.hide();
+     song = loadSound('halloween_theme.mp3');
 }
 function setup(){
     canvas = createCanvas(500,400);
     canvas.center();
-}
-function start(){
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status:- Detecting Objects";
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:- Detecting Objects";
 }
 function modelLoaded(){
     console.log("Model Loaded!");
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 function draw(){
-   image(video,0,0,500,400)
+    image(video,0,0,500,400);
+    if(status != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(video , gotResults);
+        for(i = 0; i > objects.length ; i++){
+            document.getElementById("status").innerHTML = "Status:- Object Detected";
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objets[i].x,objects[i].y);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+            if(objects[i].label == "person"){
+                document.getElementById("baby_nf").innerHTML = "Baby Found";
+                console.log("stop");
+                song.stop();
+            }
+            else{
+                document.getElementById("baby_nf").innerHTML = "Baby is not Found";
+                console.log("play");
+                song.play();
+            }
+        }
+    }
 }
