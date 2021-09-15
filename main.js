8fb05d63ff46592f308106dec406146daee26daa
NagaRithwik <89988578+NagaRithwik@users.noreply.github.com>
2021-09-15 11:06:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-function setup() {
-    canvas = createCanvas(380, 380);
+status = "";
+video = "";
+
+function preload(){
+   video = createVideo('video.mp4');
+   video.hide();
+}
+function setup(){
+    canvas = createCanvas(500,400);
     canvas.center();
-  }
-  function draw(){
-    image(video, 0, 0, 380, 380);
-  }
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status:- Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function draw(){
+   image(video,0,0,500,400)
+}
